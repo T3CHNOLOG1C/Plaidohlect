a44b6f48f36b1fc67ce7e6559313ea68b19be9d5
T3CHNOLOG1C <T3CHNOLOG1C@users.noreply.github.com>
2017-05-29 14:57:06 -0400

    add brickdurr meme

--- addons/memes.py
+++ addons/memes.py
@@ -213,6 +213,11 @@ class Memes:
     async def whatisr(self, ctx):
         """MEMES?"""
         await self._meme(ctx, "http://i.imgur.com/Z8HhfzJ.jpg")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def brickdurr(self, ctx):
+        """Memes."""
+        await self._meme(ctx, "http://i.imgur.com/hzuXOHP.png")
                 
 # Load the extension
 def setup(bot):
