fd29aa9c1d0c320f13b223cbe532c960d9ea88a9
T3CHNOLOG1C <T3CHNOLOG1C@users.noreply.github.com>
2017-06-05 19:39:39 -0400

    add pull command and add botupdate.py

---
+++ addons/botupdate.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from sys import argv
+from subprocess import call
+
+Class botupdate:
+    """
+    Bot Updating Commands (Owners Only)
+    """
+    @commands.has_permissions(administrator=True)
+    @commands.command(pass_context=True, name="pull")
+    async def pull(self, ctx):
+       """Pull from GitHub (Owner Only)"""
+        call(['git', 'pull'])
+
+def setup(bot):
+    bot.add_cog(botupdate(bot))
