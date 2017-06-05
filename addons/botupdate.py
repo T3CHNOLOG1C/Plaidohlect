43513f018e935a40c3f6d13d00c9b3406a327335
T3CHNOLOG1C <T3CHNOLOG1C@users.noreply.github.com>
2017-06-05 19:44:40 -0400

    add message after pull command

--- addons/botupdate.py
+++ addons/botupdate.py
@@ -12,6 +12,7 @@ class botupdate:
     async def pull(self, ctx):
        """Pull from GitHub (Owner Only)"""
         call(['git', 'pull'])
+    await bot.send_message(ctx.message.channel, "Changes pulled from Github :ok_hand:".format(ctx.command.name))
 
 def setup(bot):
     bot.add_cog(botupdate(bot))
