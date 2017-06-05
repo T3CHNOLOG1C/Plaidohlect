2d19a29733d9e0e11b3d874773631e811782ba1b
T3CHNOLOG1C <T3CHNOLOG1C@users.noreply.github.com>
2017-06-05 19:58:46 -0400

    add os import for later on

--- addons/botupdate.py
+++ addons/botupdate.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 from sys import argv
 from subprocess import call
@@ -10,7 +11,7 @@ class botupdate:
     def __init__(self, bot):
         self.bot = bot
         print('Addon "{}" loaded'.format(self.__class__.__name__))
-        
+
     @commands.has_permissions(administrator=True)
     @commands.command(pass_context=True, name="pull")
     async def pull(self, ctx):
